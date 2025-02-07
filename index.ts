1320dc74c2d009be796a9c579b93d003324389bd
yd-tw <yd960528@gmail.com>
2025-02-07 12:08:58 +0800

    add path config

--- index.ts
+++ index.ts
@@ -2,25 +2,25 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 
-const postsDirectory = path.join(process.cwd(), "posts");
+const DEFAULT_POSTS_PATH = "posts";
 
-export function getAllPostSlugs() {
-  return fs.readdirSync(postsDirectory);
+export function getAllPostSlugs(filePath: string = DEFAULT_POSTS_PATH) {
+  return fs.readdirSync(path.join(process.cwd(), filePath));
 }
 
-export function getAllPosts() {
-  const slugs = getAllPostSlugs();
-  return slugs.map((slug) => getPostBySlug(slug));
+export function getAllPosts(filePath: string = DEFAULT_POSTS_PATH) {
+  const slugs = getAllPostSlugs(filePath);
+  return slugs.map((slug) => getPostBySlug(filePath, slug));
 }
 
-export function getAllPostParams(){
-  const slugs = getAllPostSlugs();
+export function getAllPostParams(filePath: string = DEFAULT_POSTS_PATH) {
+  const slugs = getAllPostSlugs(filePath);
   return slugs.map((slug) => ({ slug: slug.replace(/\.md$/, "") }));
 }
 
-export function getPostBySlug(slug: string) {
+export function getPostBySlug(filePath: string = DEFAULT_POSTS_PATH, slug: string) {
   const realSlug = slug.replace(/\.md$/, "");
-  const fullPath = path.join(postsDirectory, `${realSlug}.md`);
+  const fullPath = path.join(process.cwd(), filePath, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
